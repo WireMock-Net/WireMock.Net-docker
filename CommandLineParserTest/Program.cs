b497784cd19ffd24ba21acc3b8f8440775a73938
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-08-21 17:50:27 +0200

    Test for CommandLineParser

---
+++ CommandLineParserTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace CommandLineParserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo _cultureInfo = CultureInfo.InvariantCulture;
+
+            string stringValue = "12";
+
+            Type type = typeof(int?);
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                type = Nullable.GetUnderlyingType(type);
+            }
+
+            MethodInfo mi = type.GetMethod("Parse", new[] { typeof(string), typeof(CultureInfo) });
+
+            if (mi != null)
+            {
+                if (mi.IsStatic && mi.ReturnType == type)
+                {
+                    object x = mi.Invoke(null, new object[] { stringValue, _cultureInfo });
+                    Console.WriteLine($"x1 = '{x}'");
+                }
+            }
+
+            stringValue = null;
+
+            type = typeof(int?);
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                type = Nullable.GetUnderlyingType(type);
+            }
+
+            mi = type.GetMethod("Parse", new[] { typeof(string), typeof(CultureInfo) });
+
+            if (mi != null)
+            {
+                if (mi.IsStatic && mi.ReturnType == type)
+                {
+                    object x = stringValue != null ? mi.Invoke(null, new object[] { stringValue, _cultureInfo }) : null;
+                    Console.WriteLine($"x2 = '{x}'");
+                }
+            }
+        }
+    }
+}
