3638228d38087d150bfd761aa15ff8400b589e4c
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-07-15 11:14:35 +0200

    1.5.32

--- StandAlone.NETCoreApp/create.ps1
+++ StandAlone.NETCoreApp/create.ps1
@@ -1,4 +1,4 @@
 docker build -t sheyenrath/wiremock.net -f .\Dockerfile .
-docker tag sheyenrath/wiremock.net:latest sheyenrath/wiremock.net:1.5.31
+docker tag sheyenrath/wiremock.net:latest sheyenrath/wiremock.net:1.5.32
 docker rmi $(docker images -f "dangling=true" -q)
 docker images
